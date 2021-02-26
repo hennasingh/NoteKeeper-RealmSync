98374c892e9e344e41ccf850a5fca920d6e61500
hennasingh <hennasingh04@gmail.com>
2021-02-26 17:28:48 +0000

    modified Note model class to RealmObject sub-class

--- app/src/main/java/com/geek/notekeeper/model/Note.kt
+++ app/src/main/java/com/geek/notekeeper/model/Note.kt
@@ -1,9 +1,18 @@
 package com.geek.notekeeper.model
 
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import org.bson.types.ObjectId
 import java.util.*
 
 class Note(
     var noteName: String,
-    var date: Date = Date()
-)
+    var date: Date = Date(),
+    var _partition: String = "Public"
+) : RealmObject() {
+
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+
+}
 
